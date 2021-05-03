26549eab1b8c6823629cbbab764f69d3c66c268d
Stephen Neal <stephen.neal14@gmail.com>
2021-05-03 12:40:42 -0400

    MAKE `PostOfficeServiceProvider` that publishes configs & views
    ADD registering of the `PostOfficeServiceProvider` in `TestCase`.
    ADD orchestra/testbench to composer dev requirements.

---
+++ tests/TestCase.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Sfneal\PostOffice\Tests;
+
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Sfneal\PostOffice\Providers\PostOfficeServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Register package service providers.
+     *
+     * @param Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            PostOfficeServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+
+        // Set config values
+        $app['config']->set('app.debug', true);
+    }
+}

40f109aad7b8fda7a87b8a1c59c0f24bcff7402e
Stephen Neal <stephen.neal14@gmail.com>
2021-05-03 12:52:36 -0400

    MAKE `ConfigTest` & `ViewsTest` with empty bodies

---
+++ tests/Feature/ConfigTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Sfneal\PostOffice\Tests\Feature;
+
+
+use Sfneal\PostOffice\Tests\TestCase;
+
+class ConfigTest extends TestCase
+{
+    // todo: add test methods
+}
---
+++ tests/Feature/ViewsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Sfneal\PostOffice\Tests\Feature;
+
+
+use Sfneal\PostOffice\Tests\TestCase;
+
+class ViewsTest extends TestCase
+{
+    // todo: add test methods
+}

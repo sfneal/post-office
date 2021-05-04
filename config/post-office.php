723bf18aeaf89dc5eaef4bb358bc9eb8ab08284e
Stephen Neal <stephen.neal14@gmail.com>
2021-05-04 11:26:51 -0400

    ADD `queue` & `driver` keys to post-office config
    ADD test methods to `ConfigTest`.

--- config/post-office.php
+++ config/post-office.php
@@ -1,5 +1,27 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | PostOffice Jobs Queue
+    |--------------------------------------------------------------------------
+    |
+    | Specify a Job queue to use when dispatching PostOffice jobs.  Creating a
+    | 'mail' queue can be effective for avoiding bottlenecks.
+    |
+    | type     : string
+    | default  : 'default'
+    |
+    */
+    'queue' => env('POST_OFFICE_QUEUE', 'default'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | PostOffice Queue Driver
+    |--------------------------------------------------------------------------
+    |
+    | Specify a Queue Driver for dispatching PostOffice jobs.
+    |
+    */
+    'driver' => env('POST_OFFICE_QUEUE_DRIVER', config('queue.default')),
 ];

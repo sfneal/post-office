80da3d58b9918917485e68f1a3536b4bf31e6907
Stephen Neal <stephen.neal14@gmail.com>
2021-05-05 10:54:17 -0400

    ADD tests for testing 'post-office.mailables' & 'post-office.mailables.footer' config values

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -8,6 +8,8 @@ class ConfigTest extends TestCase
     public function config_is_accessible()
     {
         $this->assertIsArray(config('post-office'));
+        $this->assertIsArray(config('post-office.mailables'));
+        $this->assertIsArray(config('post-office.mailables.footer'));
     }
 
     /** @test */
@@ -25,6 +27,45 @@ class ConfigTest extends TestCase
     {
         $expected = config('queue.default');
         $output = config('post-office.driver');
+
+        $this->assertIsString($output);
+        $this->assertSame($expected, $output);
+    }
+
+    /** @test */
+    public function mailables_view()
+    {
+        $expected = 'post-office::email';
+        $output = config('post-office.mailables.view');
+
+        $this->assertIsString($output);
+        $this->assertSame($expected, $output);
+    }
+
+    /** @test */
+    public function mailables_footer_enabled()
+    {
+        $output = config('post-office.mailables.footer.enabled');
+
+        $this->assertIsBool($output);
+        $this->assertTrue($output);
+    }
+
+    /** @test */
+    public function mailables_footer_address()
+    {
+        $expected = '35 Main Street, Milford MA 01747';
+        $output = config('post-office.mailables.footer.address');
+
+        $this->assertIsString($output);
+        $this->assertSame($expected, $output);
+    }
+
+    /** @test */
+    public function mailables_footer_unsubscribe_route()
+    {
+        $expected = 'unsubscribe';
+        $output = config('post-office.mailables.footer.unsubscribe_route');
 
         $this->assertIsString($output);
         $this->assertSame($expected, $output);
